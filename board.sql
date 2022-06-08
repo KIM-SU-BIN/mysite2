2c3c900727765e0ef85f20695c25dcb7e1c8686d
subintoto <subintoto@naver.com>
2022-06-08 15:40:19 +0900

    board.SQL

---
+++ board.sql
@@ -0,0 +1,56 @@
+-- 삭제 --
+drop table board;
+drop sequence seq_board_no;
+
+-- 테이블 삭제 --
+DROP TABLE board;
+
+
+-- 테이블 생성 --
+create table board (
+    no      number,
+    title   varchar2(500) not null,
+    content varchar2(4000),
+    hit     number,
+    reg_date date not null,
+    user_no number not null,
+    primary key(no),
+    CONSTRAINT fk_user_no foreign key(no) 
+    references users(no)
+    );
+    
+        
+-- 시퀀스 생성 --
+create sequence seq_board_no
+increment by 1
+start with 1
+nocache;
+
+
+-- 인서트 --
+insert into board
+values(seq_board_no.nextval, '제목', '~ 글 ~', 1, sysdate, 1);
+
+
+-- 조회 --
+select  no,
+        title,
+        content,
+        hit,
+        to_char(reg_date,'YY-MM-DD HH24:MI') "reg_date",
+        user_no,
+        users.name
+from board, users
+where board.user_no = users.no
+order by no desc;
+
+
+SELECT * 
+FROM users;
+
+
+-- 커밋 --
+commit;
+
+-- 롤백 --
+rollback;
